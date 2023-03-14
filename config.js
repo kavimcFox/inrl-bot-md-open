8073915fc9d5ecb1bf66c6e8135b2ee83f8210db
kavimcFox <124341107+kavimcFox@users.noreply.github.com>
2023-03-14 22:18:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 module.exports = {
     VERSION: 'V 1.0.0', // bot version
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~qQgZDUCjWfLszt3ZSN7xA8i88krQ2Dce7bRY', //your ssid to run bot
     MONGO_URL : process.env.MONGO_URL,//must be enter your mongo url;
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
